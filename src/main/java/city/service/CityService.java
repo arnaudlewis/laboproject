fe0899702fa6df3e2409a6cdc88a06184a8be037
Arnaud <arnaud.lewis@icloud.com>
2014-03-31 03:45:56 +0200

    creation autocomplete et proposition de voyage

--- src/main/java/city/service/CityService.java
+++ src/main/java/city/service/CityService.java
@@ -3,7 +3,7 @@ package city.service;
 
 import city.City;
 import city.dataAccess.DaoCity;
-import city.dto.AutocompleteDTO;
+import city.dto.AutocompletionDTO;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -15,7 +15,7 @@ public class CityService {
     @Path("/load")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public List<City> loadCities(AutocompleteDTO request) {
+    public List<City> loadCities(AutocompletionDTO request) {
         return DaoCity.getInstance().loadCities(request.getSearchParam());
     }
 }
